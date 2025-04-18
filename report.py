270a671d4229c320aa37857e4b53bd824daf2c6e
Arnoldt <visagie.arnoldt@gmail.com>
2025-04-19 01:37:07 +0200

    Titel change

--- report.py
+++ report.py
@@ -16,7 +16,7 @@ plt.rcParams['axes.titlesize'] = 12
 plt.rcParams['axes.labelsize'] = 10
 
 # Set page config
-st.set_page_config(page_title="Insident Spoorder", layout="wide")
+st.set_page_config(page_title="Insident Verslag", layout="wide")
 
 # Load and preprocess learner data
 @st.cache_data
